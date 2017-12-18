4607c7d6aabf9dcddb8c4c4840e59bb26b122e00
ZiaWang <ziawang_python@163.com>
2017-12-18 18:41:51 +0800

    CONTENT: first commit

---
+++ page_divider/urls.py
@@ -0,0 +1,26 @@
+"""page_divider URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+
+from pagination import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('list/', views.list_host),
+    re_path('edit/(?P<host_id>\d+)/', views.edit_host, name='edit_host'),
+    # path('insert_value/', views.insert_value),
+]
